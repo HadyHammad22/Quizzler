2ff30c110b33e9e791c188a9e0096d6a2034e4b7
HadyHammad22 <hady.fci@gmail.com>
2020-02-11 04:35:58 -0800

    solve some issues

--- Quizzler/ScoreVC.swift
+++ Quizzler/ScoreVC.swift
@@ -10,17 +10,32 @@ import UIKit
 
 class ScoreVC: UIViewController{
     
+    // MARK :- Outlets
     @IBOutlet weak var highScoreLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var scoreImage: UIImageView!
+    @IBOutlet weak var highScoreBackView: UIView!
+    @IBOutlet weak var highScoreView: UIView!
     
+    // MARK :- Instance variables
     var score:Int = 0
     var category:String = ""
+    
+    // MARK :- LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupComponents()
+    }
+    
+    // MARK :- SetupUI
+    func setupComponents(){
+        highScoreBackView.addCornerRadius(15)
+        highScoreView.addCornerRadius(20)
+        highScoreView.addNormalShadow()
         updateScoreUI(score: score, category: category)
     }
     
+    // MARK :- Actions
     @IBAction func buRestart(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let chooseCategory = storyboard.instantiateViewController(withIdentifier: "ChooseCategory") as? ChooseCategory
