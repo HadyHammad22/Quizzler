22d37f9d85bb5ec3f3ed990a92370d134bcb68a3
HadyHammad22 <hady.fci@gmail.com>
2020-02-11 02:08:23 -0800

    add category cell

---
+++ Quizzler/CategoryCVC.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCVC.swift
+//  Quizzler
+//
+//  Created by Hady Hammad on 2/11/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
